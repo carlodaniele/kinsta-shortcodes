8bc3381bbb2e6455bc1e2d6a4bad84f4d1b16f59
carlodaniele <carlo.daniele@gmail.com>
2017-03-21 20:30:17 +0100

    Update kinsta-shortcodes.php

--- kinsta-shortcodes.php
+++ kinsta-shortcodes.php
@@ -19,7 +19,14 @@ Author URI: http://carlodaniele.it/en/
  * @param callable 	$func 	Hook to run when shortcode is found 
  * @link https://codex.wordpress.org/Function_Reference/add_shortcode
  */
-add_shortcode( 'kinsta_btn', 'kinsta_button' );	
+
+
+function kinsta_shortcodes_init(){
+	add_shortcode( 'kinsta_btn', 'kinsta_button' );
+	add_shortcode( 'kinsta_btn_adv', 'kinsta_button_adv' );
+}
+add_action('init', 'kinsta_shortcodes_init');
+
 
 /**
  * Register a shortcode
@@ -27,14 +34,16 @@ add_shortcode( 'kinsta_btn', 'kinsta_button' );
  * @param array $atts Array of shortcode attributes
  */
 function kinsta_button( $atts ){
+
+	// normalize attribute keys, lowercase
+	$atts = array_change_key_case( (array)$atts, CASE_LOWER );
+	
 	extract( shortcode_atts(
 			array(
 				'href'		=> '#',
 				'id'		=> '',
 				'class'		=> 'green',
 				'target'	=> '',
-				'rel'		=> '',
-				'title'		=> '',
 				'label'		=> 'Button'
 			),
 			$atts,
@@ -47,9 +56,56 @@ function kinsta_button( $atts ){
 		$link_target = '';
 	}
 
-	$output = '<a href="' . $href . '" id="' . $id . '" class="button ' . $class . '"' . $link_target . '">' . $label . '</a>';
+	$output = '<p><a href="' . esc_url( $href ) . '" id="' . $id . '" class="button ' . $class . '"' . $link_target . '">' . $label . '</a></p>';
 	return $output;
 }
+
+function kinsta_button_adv( $atts, $content = null, $tag = '' ){
+
+	// normalize attribute keys, lowercase
+	$atts = array_change_key_case( (array)$atts, CASE_LOWER );
+
+	extract( shortcode_atts(
+			array(
+				'href'		=> '#',
+				'id'		=> '',
+				'class'		=> 'green',
+				'target'	=> ''
+			),
+			$atts,
+			'kinsta_btn'
+		) );
+
+	if( in_array( $target, array( '_blank', '_self', '_parent', '_top' ) ) ){
+		$link_target = ' target="' . $target . '"';
+	}else{
+		$link_target = '';
+	}
+
+	$output = '<p><a href="' . esc_url( $href ) . '" id="' . $id . '" class="button ' . $class . '"' . $link_target . '">' . $content . '</a></p>';
+	return $output;
+}
+
+function kinsta_editor_button(){
+	if ( wp_script_is( 'quicktags' ) ) {
+		?>
+		<script type="text/javascript">
+
+		QTags.addButton( 'kbtn', 'k-button', kinsta_print_btn, '', '', 'Kinsta Button', 999 );
+
+		function kinsta_print_btn(){
+			var label = prompt( 'Button label:', '' );
+
+			if ( label && label !== '' ) {
+				QTags.insertContent('[kinsta_btn_adv href="" id="" class="" target=""]' + label + '[/kinsta_btn_adv]' );
+			}
+		}
+		</script>
+		<?php
+	}
+}
+add_action( 'admin_print_footer_scripts', 'kinsta_editor_button' );
+
 
 /**
  * Enqueue scripts and styles
@@ -63,7 +119,21 @@ function kinsta_enqueue_scripts() {
 		// see https://codex.wordpress.org/Function_Reference/plugin_dir_url
 		wp_register_style( 'kinsta-stylesheet',  plugin_dir_url( __FILE__ ) . 'css/style.css' );
 		// see https://developer.wordpress.org/reference/functions/wp_enqueue_style/
-    	wp_enqueue_style( 'kinsta-stylesheet' );
+    		wp_enqueue_style( 'kinsta-stylesheet' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'kinsta_enqueue_scripts');
+
+// Add buttons to TinyMCE toolbar
+function kinsta_register_mce_buttons( $buttons ) {
+	$buttons[] = 'kinsta';
+	return $buttons;
+}
+add_filter( 'mce_buttons', 'kinsta_register_mce_buttons' );
+
+// Add a custom TinyMCE plugin
+function kinsta_register_mce_plugin( $plugin_array ) {
+   $plugin_array['kinsta'] = plugins_url( '/mce/kinsta/plugin.js', __FILE__ );
+   return $plugin_array;
+}
+add_filter( 'mce_external_plugins', 'kinsta_register_mce_plugin' );
